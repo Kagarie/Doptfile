4a4aeac87058dd6252df63c81b7476226c7aa908
Kagari <aymeric.adenot27@orange.fr>
2020-10-19 23:37:02 +0000

    correction chemin

--- start/checkEcran.sh
+++ start/checkEcran.sh
@@ -3,7 +3,7 @@
 ecran="eDP-1 connected primary 1920x1080+0+0 (normal left inverted right x axis y axis) 381mm x 214mm"
 commande=$(xrandr | grep 'connected primary 1920x1080+0+0')
 if [ "$commande" = "$ecran" ];then
-	./wallpaper
+	./.start/wallpaper
 else
 	nitrogen --restore &
 fi
